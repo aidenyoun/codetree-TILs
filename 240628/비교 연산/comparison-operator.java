4deddfe012a8dd0fb73209e671ece2bdc9d10cf8
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 21:20:42 +0900

    Title: 비교 연산, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240628/비교 연산/comparison-operator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if (a >= b) {
+            System.out.printf("1\n");
+        }
+        else {
+            System.out.printf("0\n");
+        }
+        if (a > b) {
+            System.out.printf("1\n");
+        }
+        else {
+            System.out.printf("0\n");
+        }   
+        if (b >= a) {
+            System.out.printf("1\n");
+        }
+        else {
+            System.out.printf("0\n");
+        }
+        if (b > a) {
+            System.out.printf("1\n");
+        }
+        else {
+            System.out.printf("0\n");
+        }
+        if (a == b) {
+            System.out.printf("1\n");
+        }
+        else {
+            System.out.printf("0\n");
+        }
+        if (a != b) {
+            System.out.printf("1\n");
+        }
+        else {
+            System.out.printf("0\n");
+        }                      
+    }
+}
