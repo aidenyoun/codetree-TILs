dcdfa46ab6e0c4e8ad246eca11b5f29033ec6c0c
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 15:58:17 +0900

    Title: 세 실수의 반올림, Time: 175ms, Memory: 11MB, Status: Passed - CodeTree

--- 240628/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 240628/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -7,8 +7,8 @@ public class Main {
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         double c = sc.nextDouble();
-        System.out.printf("%.3f",a);
-        System.out.printf("%.3f",b);
+        System.out.printf("%.3f\n",a);
+        System.out.printf("%.3f\n",b);
         System.out.printf("%.3f",c);
         
         
