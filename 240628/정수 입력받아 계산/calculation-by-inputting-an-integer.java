b5a87487cb424faaaa220038a191afb5c7fc6965
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 15:21:48 +0900

    Title: 정수 입력받아 계산, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240628/정수 입력받아 계산/calculation-by-inputting-an-integer.java
+++ 240628/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throw IOException{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         long a = sc.nextLong();
         System.out.printf("%d",a*2+3);
