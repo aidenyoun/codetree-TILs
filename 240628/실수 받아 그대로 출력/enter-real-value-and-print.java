2770a3cc0182148adff8553646918d4a825191ea
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 15:23:36 +0900

    Title: 실수 받아 그대로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240628/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 240628/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -5,6 +5,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         double n = sc.nextDobule();
-        System.out.prtinf(%.2f,n);
+        System.out.printnf(%.2f,n);
     }
 }
