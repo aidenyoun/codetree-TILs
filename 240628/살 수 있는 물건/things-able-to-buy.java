4fda93c0452c7021dff7549c32b7cc52b6883be2
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 20:32:38 +0900

    Title: 살 수 있는 물건, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240628/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        if (a >= 3000) {
+            System.out.printf("book");
+        }
+        else if (a >= 1000) {
+            System.out.printf("mask");
+        }
+        else {
+            System.out.printf("no");
+        }
+    }
+}
