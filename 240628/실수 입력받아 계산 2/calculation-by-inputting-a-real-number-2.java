0f666ae4e8a83b79f9afa66c7b97246d0328ce65
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 15:39:11 +0900

    Title: 실수 입력받아 계산 2, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

--- 240628/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 240628/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        double a = sc.nextDobule();
+        double a = sc.nextDouble();
         System.out.printf("%.2f",a+1.5);
 
     }
