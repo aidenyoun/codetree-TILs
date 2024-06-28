69a81f6e2d2642f92dd5ef8e870b11159c49667c
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 18:13:12 +0900

    Title: 큰 수에서 빼기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240628/큰 수에서 빼기/subtract-from-large-number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if (a > b) {
+            System.out.printf("%d",a-b);
+        }
+        else {
+            System.out.printf("%d",b-a);
+        }
+    }
+}
