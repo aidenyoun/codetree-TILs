ed4604b3c4fafe0ff133f46048ff3edf46a460b6
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 15:24:31 +0900

    Title: 실수 받아 그대로 출력, Time: 144ms, Memory: 11MB, Status: Passed - CodeTree

--- 240628/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 240628/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextDobule();
+        double n = sc.nextDouble();
         System.out.printf("%.2f",n);
     }
 }
