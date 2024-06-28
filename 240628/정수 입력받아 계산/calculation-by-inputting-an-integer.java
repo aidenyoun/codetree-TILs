853f29d0c579a6b7b55d2064216990946d53cd39
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 15:21:20 +0900

    Title: 정수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240628/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throw IOException{
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        System.out.printf("%d",a*2+3);
+
+    }
+}
