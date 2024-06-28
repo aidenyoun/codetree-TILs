df3e3775900b96c52422627c5b3b99acaf85c786
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 16:03:06 +0900

    Title: 문자열 받아 출력, Time: 222ms, Memory: 10MB, Status: Passed - CodeTree

--- 240628/문자열 받아 출력/enter-word-and-print.java
+++ 240628/문자열 받아 출력/enter-word-and-print.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        System.out.printf("%s", s));
+        System.out.printf("%s", s);
     }
 }
