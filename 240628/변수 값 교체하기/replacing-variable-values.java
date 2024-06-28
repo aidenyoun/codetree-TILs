baf9c931265febc2c3688e4f3fcc19f3ef3cdfd6
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 14:51:37 +0900

    Title: 변수 값 교체하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

--- 240628/변수 값 교체하기/replacing-variable-values.java
+++ 240628/변수 값 교체하기/replacing-variable-values.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         long a = 3;
-        long a = 6;
+        a = 6;
         System.out.printf("%d",a);
     }
 }
