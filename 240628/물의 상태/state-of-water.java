cb5e096741f8fa7fa8eef15ff786350af013884e
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-28 20:30:21 +0900

    Title: 물의 상태, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240628/물의 상태/state-of-water.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        if (a >= 100) {
+            System.out.printf("vapor");
+        }
+        else if (a < 0) {
+            System.out.printf("ice");
+        }
+        else {
+            System.out.printf("water");
+        }
+    }
+}
