d0d438abdacaa6f8962b47eadd5d06925952a82c
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-29 12:52:59 +0900

    Title: 차례로 출력, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240629/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        long a = 5;
+        for (long i = a ; i <= 17 ; i++) {
+            System.out.printf("%d ",i);
+        }
+    }
+}
