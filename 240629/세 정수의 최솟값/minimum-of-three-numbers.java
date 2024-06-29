a5e5180a95d07f1fb82b7a71451f9d2dfe8f526d
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-29 10:46:42 +0900

    Title: 세 정수의 최솟값, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240629/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        if (a <= b && a <= c) {
+            System.out.printf("%d",a);
+        }
+        else if (b <= a && b <= c) {
+            System.out.printf("%d",b);
+        }
+        else if (c <= a && c <= b) {
+            System.out.printf("%d",c);
+        }        
+    }
+}
