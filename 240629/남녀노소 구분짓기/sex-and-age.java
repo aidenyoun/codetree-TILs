3daa7f0d9f5ebd961097a5b17775ffa848520516
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-29 12:11:47 +0900

    Title: 남녀노소 구분짓기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240629/남녀노소 구분짓기/sex-and-age.java
+++ 240629/남녀노소 구분짓기/sex-and-age.java
@@ -7,11 +7,11 @@ public class Main {
         long age = sc.nextLong();
         if (sex == 0) {
             if (age >= 19) System.out.printf("MAN");
-            else System.out.printf("BOY")
+            else System.out.printf("BOY");
         }
         else {
             if (age >= 19) System.out.printf("WOMAN");
-            else System.out.printf("GIRL")            
+            else System.out.printf("GIRL");
         }
     }
 }
