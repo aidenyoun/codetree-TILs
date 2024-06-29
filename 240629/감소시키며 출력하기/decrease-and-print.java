b761604a758606ee3d67ec69fdfdc0aab6a9b691
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-06-29 17:27:22 +0900

    Title: 감소시키며 출력하기, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240629/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for (long i = 17; i <= 5; i--){
+            System.out.printf("%d ", i);
+        }
+    }
+}
