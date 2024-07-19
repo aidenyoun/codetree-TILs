024db7b623894efe8977f572b74c3d690a5f4b3a
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-07-19 14:42:16 +0900

    Title: 중앙값 계산 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/중앙값 계산 2/get-median-2.py
+++ 240719/중앙값 계산 2/get-median-2.py
@@ -1,15 +1,11 @@
 n = int(input())
 l = list(map(int, input().split()))
+temp = []
 ans = []
-temp = 0
-temp1 = 0
 for i in range(len(l)):
-    if i == 0:
-        ans.append(l[i])
-        temp += l[i]
-        temp1 += 1
-    elif i % 2 == 0:
-        temp += l[i]
-        temp1 += 1
-        ans.append(temp//temp1)
+    temp.append(l[i])
+    if i % 2 == 0:
+        temp.sort()
+        ans.append(temp[(i//2)])
+
 print(*ans)
