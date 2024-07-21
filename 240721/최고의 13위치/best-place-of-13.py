3ba96eaca0f6a65f424d91e12b6c4b9b5ea037c6
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-07-21 14:05:48 +0900

    Title: 최고의 13위치, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240721/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,23 @@
+n = int(input())
+temp = []
+temp1 = 0
+max_temp = 0
+ans = 0
+for i in range(n):
+    temp.append(list(map(int, input().split())))
+
+if len(temp) == 3:
+    for i in range(len(temp)):
+        temp1 = temp[i].count(1)
+        max_temp = max(temp1, max_temp)
+        ans = max_temp
+
+else:
+    for i in range(len(temp)):
+        for j in range(0, len(temp)%3):
+            if temp[i][j] == 1:
+                temp1 += 1
+        max_temp = max(temp1, max_temp)
+        ans = max_temp   
+
+print(ans)
