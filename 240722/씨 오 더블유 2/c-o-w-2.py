757c65adcdc20cb49cb1d5a9dde6b987e8c313b3
Aiden Youn <78311775+aidenyoun@users.noreply.github.com>
2024-07-22 09:47:44 +0900

    Title: 씨 오 더블유 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/씨 오 더블유 2/c-o-w-2.py
+++ 240722/씨 오 더블유 2/c-o-w-2.py
@@ -1,9 +1,9 @@
 n = int(input())
 a = list(input())
 ans = 0
-for i in range(len(a)):
-    for j in range(i+1, len(a)):
-        for k in range(i+2, len(a)):
+for i in range(len(a) - 2):
+    for j in range(i+1, len(a) - 1):
+        for k in range(j+1, len(a)):
             if a[i] == 'C' and a[j] == 'O' and a[k] == 'W':
                 ans += 1
 print(ans)
